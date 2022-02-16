89c18d10ba4e0517b15444ea18c4395e8334f195
Rory James <rory.allen.james@gmail.com>
2022-02-15 19:38:26 -0500

    Added minor variance

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ import numpy as np
 @st.cache
 def get_data():
     df = pd.read_excel('data/combined_files_output.xlsx')
-    df = df.rename(columns={'Latitude':'lat','Longitude':'lon'})
+    df = df.rename(columns={'Latitude':'lat','Longitude':'lon',
+    'Minor Variance': 'Minor_Variance'}) 
     return df
 
 df = get_data()
 
 st.title('Development Applications in the City of Toronto')
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     status = st.multiselect("Status", df.Status.unique(),
@@ -30,8 +31,16 @@ if consent == 'Yes':
 else: 
     consent = 0
 
+with col4:
+    minor_variance = st.selectbox("Minor Variance", ('Yes','No'))
+
+if minor_variance == 'Yes':
+    minor_variance = 1
+else: 
+    minor_variance = 0
 
 #Query the data based on the user parameters
-df_selection = df.query("Status == @status & Council == @council & Consent == @consent" )
+df_selection = df.query("Status == @status & Council == @council & Consent == @consent & Minor_Variance == @minor_variance" )
 
 st.map(df_selection)
+st.write(df_selection)
