9a42607d350bb43abbe42c9c7e0691a7e690deba
Rory James <rory.allen.james@gmail.com>
2022-02-16 17:01:27 -0500

    Fixed layout and added app description

--- explore_page.py
+++ explore_page.py
@@ -10,18 +10,27 @@ def get_data():
 df = get_data()
 
 st.title('Development Applications in the City of Toronto')
+st.markdown("""This interactive dashboard is designed for visualizing development applications in the City of Toronto. Specifically, the intent of the dashboard is to give users the
+functionality to visualize applications based on their decision status, and the various parameters of the applications. All of the data for this project was sourced from
+ the [City of Toronto Open Data Portal](https://open.toronto.ca/).
+""")
 
-col1, col2, col3, col4 = st.columns(4)
+##Row 1
 
-with col1:
-    status = st.multiselect("Status", df.Status.unique(),
-    default=df.Status.unique())
+row1_col1, row1_col2 = st.columns([0.6, 2])
 
-with col2:
+with row1_col1:
+    status = st.selectbox("Status", df.Status.unique())
+    
+with row1_col2:
     council = st.multiselect("Council", df.Council.unique(),
     default=df.Council.unique())
 
-with col3:
+ ## Row 2   
+
+row2_col1, row2_col2, row2_col3 = st.columns([0.6, 1.4, 2])
+
+with row2_col1:
     consent = st.selectbox("Consent", ('Yes','No'))
 
 if consent == 'Yes':
@@ -29,7 +38,7 @@ if consent == 'Yes':
 else: 
     consent = 0
 
-with col4:
+with row2_col2:
     minor_variance = st.selectbox("Minor Variance", ('Yes','No'))
 
 if minor_variance == 'Yes':
@@ -38,7 +47,7 @@ else:
     minor_variance = 0
 
 #Query the data based on the user parameters
-df_selection = df.query("Status == @status & Council == @council & Consent == @consent & Minor_Variance == @minor_variance" )
+df_selection = df.query("Status == @status & Council == @council & Consent == @consent & Minor_Variance == @minor_variance")
 
 st.map(df_selection)
 st.write(df_selection)
