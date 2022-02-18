120b5e0edb7041e851613d0390c38119014e6f34
Rory James <rory.allen.james@gmail.com>
2022-02-18 16:37:58 -0500

    Updated about page

--- apps/about.py
+++ apps/about.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 def show_about_page():
     
-    st.title('Development Applications in the City of Toronto')
-    st.markdown("""This interactive dashboard is designed for visualizing development applications in the City of Toronto. Specifically, the intent of the dashboard is to give users the
-    functionality to visualize applications based on their decision status, and the various parameters of the applications. All of the data for this project was sourced from
-    the [City of Toronto Open Data Portal](https://open.toronto.ca/).
+    st.title('Development Applications In The City of Toronto')
+    st.markdown("""Welcome to the City of Toronto Development Application Portal! This is a proof of concept application that is intended to give users the ability
+                to view development applications in the City of Toronto. based on their application status - either approved or denied. In addition, this app allows users to predict the likelihood that an application will receive approval based on the applications perameters.
+                All of the data for this project was sourced from 
+                the [City of Toronto Open Data Portal](https://open.toronto.ca/). The source code for this app can be found [here](https://github.com/RoryAJames/TorontoDevelopmentApplications).
     """)
--- apps/explore_page.py
+++ apps/explore_page.py
@@ -14,28 +14,31 @@ df = get_data()
 
 def show_explore_page():
 
-    st.title('Explore Development Applications in the City of Toronto')
+    st.title('Explore Development Applications In The City of Toronto')
+    st.write('Note - This dashboard only shows applications based on their status as of May 3rd 2017.')
+    st.write('')
 
 ##Row 1
-    row1_col1, row1_col2, row1_col3 = st.columns([0.6, 1, 2])
+    row1_col1, row1_col2, row1_col3 = st.columns([1, 1.5, 2])
 
     with row1_col1:
-        status = st.selectbox("Status", df.Status.unique())
+        status = st.selectbox("Select The Application Status", df.Status.unique())
 
     with row1_col2:
-        num_props = st.selectbox("Number of Properties", df.Number_of_Properties.unique())
+        num_props = st.multiselect("Select The Number of Properties", df.Number_of_Properties.unique())
     
     with row1_col3:
-        council = st.multiselect("Council", df.Council.unique())
+        council = st.multiselect("Select The Council(s) Of Interest", df.Council.unique())
 
  ## Row 2   
 
-    row2_col1, row2_col2 = st.columns([1, 2])
+    row2_col1, row2_col2 = st.columns([1.8, 1.8])
 
     with row2_col1:
-        application_type = st.multiselect("Application Type", ('Consent','Minor Variance',
+        application_type = st.multiselect("Select The Application Type(s)", ('Consent','Minor Variance',
         'Official Plan Rezoning', 'Site Plan Application'))
 
+    #Check what has been selected from application type
     
     if 'Consent' in application_type:
         consent = 1
@@ -47,12 +50,18 @@ def show_explore_page():
     else:
         minor_variance = 0
 
+    if 'Official Plan Rezoning' in application_type:
+        official_plan = 1
+    else:
+        official_plan = 0
+    
+    if 'Site Plan Application' in application_type:
+        site_plan = 1
+    else:
+        site_plan = 0
         
-    ##### Need to update the names of official plan and site plan with _
-    
-    
     with row2_col2:
-        house_in_need = st.slider("Number of Households In Core Housing Need",value=(100,6000))
+        house_in_need = st.slider("Show Applications In Range of Households In Core Housing Need",value=(100,6000))
         house_in_need_lower = house_in_need[0]
         house_in_need_upper = house_in_need[1]
     
@@ -64,10 +73,11 @@ def show_explore_page():
     & Council == @council \
     & Consent == @consent \
     & Minor_Variance == @minor_variance \
+    & Official_Plan_Rezoning == @official_plan \
+    & Site_Plan_Application == @site_plan \
     & Households_In_Core_Housing_Need >= @house_in_need_lower \
     & Households_In_Core_Housing_Need <= @house_in_need_upper\
     ''')
 
-    st.write(f"There are {len(df_selection)} applications that meet these parameters.")
+    st.subheader(f"**There are {len(df_selection)} applications that meet these parameters.**")
     st.map(df_selection)
-    st.write(df_selection)
