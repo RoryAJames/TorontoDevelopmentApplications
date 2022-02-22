32320af60e6abd9ed546a09fdb77d52f5c20297f
Rory James <rory.allen.james@gmail.com>
2022-02-22 10:44:34 -0500

    Testing prediction page functionality

--- apps/predict_page.py
+++ apps/predict_page.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas as pd
+
+@st.cache
+def get_data():
+    df = pd.read_excel('data\combined_files_output.xlsx')
+    return df
+
+df = get_data()
 
 def show_predict_page():
     
     st.title('Predict An Applications Approval Status')
     st.markdown('Please select the application perameters you are interested in predicting!')
+    status = st.selectbox("Select The Application Status", df.Zoning_Category.unique())
