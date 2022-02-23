49b4d3ac113af3a6558ce85979a4c0052415c8af
Rory James <rory.allen.james@gmail.com>
2022-02-22 20:00:13 -0500

    Updated log reg iterations

--- model_building.py
+++ model_building.py
@@ -4,7 +4,7 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
-from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
@@ -13,25 +13,28 @@ df = pd.read_csv('data\predict_page.csv')
 X = df.drop("Status", axis=1)
 y = df["Status"]
 
+#Establish the hold out set to test cross val after fitting
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
+#Create an instance of income scaler to be passed to pickle file
 income_scaler = StandardScaler()
 X_train['Average_Income'] = income_scaler.fit_transform(X_train['Average_Income'].values.reshape(-1,1))
 X_train['Average_Income'] = income_scaler.transform(X_train['Average_Income'].values.reshape(-1,1))
 
+#Since the dataset is imbalanced use stratified KFold to make equal size sets
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 pipelines = {
-    'Logistic': make_pipeline(LogisticRegression(random_state=1)),
-    'RandomForest': make_pipeline(RandomForestClassifier(random_state=1))
+    'Logistic': make_pipeline(LogisticRegression(random_state=42, max_iter=1000)),
+    'RandomForest': make_pipeline(RandomForestClassifier(random_state=42))
     }
 
 hyperparameters = {
     
-'Logistic' : { 
+'Logistic' : {
     'logisticregression__C': [0.01, 0.1, 1, 10], 
     'logisticregression__penalty' : ['none','l1', 'l2'],
-    'logisticregression__class_weight' : [None, {0:1,1:2},{0:1,1:5} ]
+    'logisticregression__class_weight' : [None, {0:1,1:2},{0:1,1:5}]
     },
 
 'RandomForest' : {
@@ -44,7 +47,7 @@ hyperparameters = {
 
 fitted_models = {} #Establishes a dictionary that will hold the model and its best performing hyperparameters
 
-# Grid search each model and hyperparameter with 5 fold cross validation
+# Grid search each model and hyperparameter with the five stratified cross validation sets
 
 for name, pipeline in pipelines.items():
     model = GridSearchCV(pipeline, 
@@ -67,5 +70,5 @@ for name, model in fitted_models.items():
    print('This models best performing parameters are ' + str(model.best_params_))
    print()
 
-model.best_params_
+print(model.best_params_)
 
