8eea4b0c4b44556d934363970ea9b0630ee76ee4
Rory James <rory.allen.james@gmail.com>
2022-03-22 13:49:33 -0400

    Updated config

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from apps.explore_page import show_explore_page
 from apps.predict_page import show_predict_page
 
 st.set_page_config(layout="wide",
-                   initial_sidebar_state="expand")
+                   initial_sidebar_state="expanded")
 
 with st.sidebar:
     selected = option_menu("Main Menu",
