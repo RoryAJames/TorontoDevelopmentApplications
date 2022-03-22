580a6d558040448a793378a874c2bdacdeca3f14
Rory James <rory.allen.james@gmail.com>
2022-03-22 11:49:57 -0400

    Updated about page

--- apps/about.py
+++ apps/about.py
@@ -18,7 +18,7 @@ def show_about_page():
     
     st.subheader("Predict The Likelihood That An Application Receives Approval")
     
-    st.write("Use Machine Learning to predict the outcome of an application! Enter an applications parameters and get an estmated likeliehood that this application will be approved.")
+    st.write("Use Machine Learning to predict the outcome of an application! Enter an applications parameters and get an estmated likeliehood that the application will be approved.")
     
     with st.expander("Click Here To See A Demo."):
         st.image("images\predictpage.gif")
